3b2e063b20c5cfab369ecc71141bf7c497448b31
Carolina <carolinarb1994@gmail.com>
2024-03-25 19:52:07 +0100

    Update files

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -6,38 +6,42 @@ const Countdown = () => {
   const [isFinished, setIsFinished] = useState(false)
 
   useEffect(() => {
-    const targetDate = new Date("2024-04-15T00:00:00")
+    const targetDate = new Date("2024-03-25T00:00:00")
     const intervalID = setInterval(() => {
       const now = new Date()
       const distance = targetDate.getTime() - now.getTime()
-      if (distance <= 0) {  
+      if (distance <= 0) {
         setIsFinished(true)
         clearInterval(intervalID)
         return
       } else {
-      const days = Math.floor(distance / (1000 * 60 * 60 * 24))
-      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
-      const seconds = Math.floor((distance % (1000 * 60)) / 1000)
-      setCountdown(`${days}d ${hours}h ${minutes}m ${seconds}s`)
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24))
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000)
+        setCountdown(`${days}d ${hours}h ${minutes}m ${seconds}s`)
       }
     }, 1000);
     return () => clearInterval(intervalID)
   }, [])
 
   return (
-    <div className="mb-4 text-5xl tracking-tight font-extrabold text-white text-center">
-        {isFinished ? 
+    <div className="py-8 px-4 mx-auto max-w-xl sm:max-w-3xl lg:max-w-4xl lg:py-16 lg:px-6 text-center">
+      {isFinished ?
         <>
-        <h2 className="text-5xl tracking-tight font-extrabold text-white text-center">Feliz Cumpleaños</h2>
-        <img src="/public/go-karts.jpg" alt="imagen Karts" />
-        <p>🏎️ Efectivamente nos vamos a los karts!! 🏎️</p>
-        <h3>Selecciona el día que quieres ir</h3>
-        <DatePickerComponent/>
+          <h2 className="mb-4 text-4xl lg:text-5xl tracking-tight font-extrabold text-white text-center ">Feliz Cumpleaños</h2>
+          <div className=' w-full sm:w-[400px] h-[336px] overflow-hidden mx-auto block pt-8 pb-8 '>
+            <img className='w-full h-full object-cover rounded-lg' src="/go-karts.jpg" alt="imagen Karts" />
+          </div>
+          <p className="mb-4 text-2xl sm:text-4xl lg:text-5xl tracking-tight font-extrabold text-white text-center">🏎️ Nos vamos a los karts!! 🏎️</p>
+          <h3 className=" text-center font-light text-gray-500 sm:text-xl dark_text-gray-400">Selecciona el día que quieres ir</h3>
+          <DatePickerComponent />
         </>
-         : countdown}
-        </div>
-       
+        : <h2 className='mb-4 text-2xl sm:text4xl lg:text-5xl tracking-tight font-extrabold text-white'>
+          {countdown}
+          </h2>
+      }
+    </div>
   )
 }
 
--- src/components/DaterPickerComponent.jsx
+++ src/components/DaterPickerComponent.jsx
@@ -15,30 +15,31 @@ const DatePickerComponent = () => {
       .then(response => console.log(response))
       .catch(error => console.log(error));
   };
-	return (
-		<div className="flex justify-center items-center">
-			<div className={`transition-all ${isVisible ? "" : "hidden"}`}>
-				<DatePicker
-					selected={selectedDate}
-					onChange={handleDateChange} // Función que se ejecuta cuando el usuario selecciona una fecha
-					dateFormat="dd/MM/yyyy"
-					minDate={new Date()}
-					isClearable
-					showYearDropdown
-					scrollableMonthYearDropdown
-					className="border p-2 rounded-lg w-full text-center text-white font-bold mt-4"
-					placeholderText="Selecciona una fecha"
-				/>
-			</div>
-			<div className="flex justify-center items-center">
-				<button
-					onClick={() => setIsVisible(!isVisible)}
-					className="font-extrabold text-gray-500 rounded sm:text-xl dark_text-gray-400 bg-white">
-					{isVisible ? "Ocultar Calendario" : "Mostrar Calendario"}
-				</button>
-			</div>
+  return (
+	<div className="flex flex-col justify-center items-center pb-9">
+		<div className={`transition-all ${isVisible ? "" : "hidden"}`} >
+			<DatePicker
+				selected={selectedDate}
+				onChange={handleDateChange} // Función que se ejecuta cuando el usuario selecciona una fecha
+				dateFormat="dd/MM/yyyy"
+				minDate={new Date()}
+				isClearable
+				showYearDropdown
+				scrollableMonthYearDropdown
+				className="border p-2 rounded-lg text-center text-sm text-white font-bold mt-4 w-full"
+				placeholderText="Selecciona una fecha"
+			/>
+	</div>
+		<div className='font-extrabold text-gray-500 rounded sm:text-xl bg-white mt-4 mb-10 w-full text-center w-[200px]'>
+			<button
+				onClick={() => setIsVisible(!isVisible)}
+				className='transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110'
+				>
+				{isVisible ? "Ocultar Calendario" : "Mostrar Calendario"}
+			</button>
 		</div>
-	);
-};
+	</div>
+	)	
+}
 
 export default DatePickerComponent;

92cf1795325b152722411525862499342700fedd
Carolina <carolinarb1994@gmail.com>
2024-03-26 19:03:24 +0100

    Add dotenv

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ import express from 'express';
 import cors from 'cors';
 import pkg from 'pg';
 
+require('dotenv').config();
+
 const app = express();
 const { Pool } = pkg;
 const pool = new Pool({

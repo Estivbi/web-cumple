a1de4a4abacff712603221c4b604f99d8074e452
Carolina <carolinarb1994@gmail.com>
2024-03-29 13:03:23 +0100

    update variables

--- server/index.js
+++ server/index.js
@@ -10,11 +10,11 @@ dotenv.config();
 const app = express();
 const { Pool } = pkg;
 const pool = new Pool({
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	host: process.env.DB_HOST,
-	port: process.env.DB_PORT,
-	database: process.env.DB_NAME,
+	user: process.env.PGUSER,
+	password: process.env.PGPASSWORD,
+	host: process.env.PGHOST,
+	port: process.env.PGPORT,
+	database: process.env.POSTGRES_DB,
 	// connectionString: process.env.DATABASE_URL,
 });
 
